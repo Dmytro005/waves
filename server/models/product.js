6f75c02221bf78dc170e348a205d308cd3ea70e5
Dmytro Harazdovskiy <harazdovskiy@gmail.com>
2018-10-14 15:47:31 +0300

    Added product schema

---
+++ server/models/product.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      maxlength: 100
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      maxlength: 100
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 255
+    },
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Brand',
+      required: true
+    },
+    shipping: {
+      type: Boolean,
+      required: true
+    },
+    available: {
+      required: true,
+      type: Boolean
+    },
+    wood: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Wood',
+      required: true
+    },
+    frets: {
+      type: Number,
+      required: true
+    },
+    sold: {
+      type: Number,
+      required: true,
+      maxlength: 255
+    },
+    publish: {
+      type: Boolean,
+      required: true
+    },
+    images: {
+      type: Array,
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { Product };
