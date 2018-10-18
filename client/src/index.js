c0f18bb26bd98197b6821dafb93e73b349b6598f
Dmytro Harazdovskiy <harazdovskiy@gmail.com>
2018-10-18 10:42:45 +0300

    [CLIENT] Brought redux into app

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './styles/styles.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
+import './styles/styles.css';
 import Routes from './routes';
+import Reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
