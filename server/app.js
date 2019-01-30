ec991bcdb42c8b447cb2324f1377937fe1fa0bfc
Dmytro005 <harazdovskiy@gmail.com>
2019-01-30 11:46:29 +0200

    Added proxy for static react app build

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true
   })
 );
